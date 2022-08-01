3085d9cac9c23f0e0109bbb2bab822e28d71ca6d
pabluwu <lopez.pablo2305@gmail.com>
2022-08-01 14:48:55 -0400

    commit change

--- src/Monocontact.php
+++ src/Monocontact.php
@@ -5,6 +5,7 @@ namespace mono;
 class Monocontact{
 
     private $api_key;
+    private $curl;
     private $api_url = 'https://api.monocontact.net/';
     public function saludo($saludo = "Hello World"){
         return $saludo;
